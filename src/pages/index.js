11506ec5dad05d79316cd8a0ba73974f28dcf3be
Adit0507 <adi05072002@gmail.com>
2024-12-12 11:45:08 +0530

    practice

---
+++ src/pages/index.js
@@ -0,0 +1,41 @@
+import Error from "./Error";
+import Home from "./Home";
+import Capsules from "./Capsules"
+import Cores from "./Cores";
+import Crew from "./Crew";
+import SingleCrew from "./SingleCrew";
+import Dragons from "./Dragons";
+import SingleDragon from "./SingleDragon";
+import Landpad from "./Landpad";
+import SingleLandPads from "./SingleLandPads";
+import Launches from "./Launches";
+import SingleLaunch from "./SingleLaunch";
+import LaunchPads from "./LaunchPads";
+import Payload from "./Payload";
+import SingleLaunchPad from "./SingleLaunchPad";
+import Roadster from "./Roadster";
+import Rocket from "./Rocket";
+import SingleRocket from "./SingleRocket";
+import Starlink from "./Starlink";
+
+export { 
+    Error, 
+    Home, 
+    Capsules, 
+    Cores, 
+    Crew, 
+    SingleCrew, 
+    Dragons,
+    SingleDragon,
+    Landpad,
+    SingleLandPads,
+    Launches,
+    SingleLaunch,
+    LaunchPads,
+    SingleLaunchPad, 
+    Payload,
+    Roadster,
+    Rocket,
+    SingleRocket,
+    Starlink
+}
